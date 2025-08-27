9ff5d25a65373e8d911a6841fa104d2195bfd871
unknown <prajjwal.jaiswal@dotsquares.com>
2025-08-27 10:37:26 +0530

    add cors

--- src/main.ts
+++ src/main.ts
@@ -49,7 +49,7 @@ async function bootstrap() {
   // });
 
   app.enableCors({
-    origin: 'http://localhost:3000', // your frontend URL
+    origin: '*', // your frontend URL
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
     credentials: true,               // allows cookies/auth headers
   });
